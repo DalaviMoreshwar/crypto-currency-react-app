8ccbe0ccc004a5b7c9755321b5f06adb94fe490c
Moreshwar Dalvi <moreshwar@anew-da.com>
2022-06-26 11:36:42 +0530

    installed axios and icons pkg and configured an API of CoinGecko

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const _URL =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
 function App() {
-  return <></>;
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(_URL)
+      .then((response) => {
+        setCoins(response.data);
+        console.log(response.data[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return <>Heeyy</>;
 }
 
 export default App;
